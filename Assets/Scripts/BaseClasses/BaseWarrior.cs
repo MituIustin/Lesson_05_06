afff64308288d00ecd823d6e7fa667710a69e3d1
MituIustin <126506678+MituIustin@users.noreply.github.com>
2024-03-30 12:27:27 +0200

    Initial commit

---
+++ Assets/Scripts/BaseClasses/BaseWarrior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWarrior : BaseClassScript
+{
+    public BaseWarrior()
+    {
+        ClassName = "Warrior";
+        Health = 10;
+        Strength = 5;
+        Intelligent = 2;
+        Agility = 3;
+        Damage = Strength * 5;
+        Shoot = false;
+    }
+
+    public void Hit()
+    {
+    }
+}
